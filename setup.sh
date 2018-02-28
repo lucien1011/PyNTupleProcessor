6eefdc1197af33de513588bcb03b1dc17be3f6d7
lucien1011 <lucien1011@gmail.com>
2018-02-28 06:22:25 -0500

    update setup to source cmssw

--- setup.sh
+++ setup.sh
@@ -2,3 +2,7 @@ export PYTHONPATH=${PYTHONPATH}:${PWD}/
 export PATH=${PATH}:${PWD}/bin/
 
 export BASE_PATH=${PWD}
+
+cd /cvmfs/cms.cern.ch/slc6_amd64_gcc630/cms/cmssw/CMSSW_9_4_4/src/ 
+eval `scramv1 runtime -sh`
+cd -
