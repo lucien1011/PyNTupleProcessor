2e9399a16c061a7ac32114dda5438b612afbbd96
lucien1011 <lucien1011@gmail.com>
2018-05-02 11:56:46 -0400

    update test_GEN-SIM.py

--- DarkZ/test_GEN-SIM.py
+++ DarkZ/test_GEN-SIM.py
@@ -3,7 +3,7 @@ from Core.Sequence import Sequence
 from Core.EndSequence import EndSequence
 from Core.OutputInfo import OutputInfo 
 
-from Core.NanoAODResult.Component import Component
+from Core.Component import Component
 
 from DarkZ.Analyzer.GENSIM import CollectionProducer
 from DarkZ.EndModule.PlotEndModule import PlotEndModule
@@ -12,7 +12,7 @@ from RPV.Analyzer.Plotter.Plot import Plot
 from Core.Utils.LambdaFunc import LambdaFunc
 
 componentList = Component(
-        "/cms/data/store/user/klo/DarkPhoton_Moriond17_GEN-SIM/v2/ZD_UpTo0j_MZD20_Eps1e-2/PUMoriond17-Realistic25ns13TeVEarly2017Collision-93X_mc2017_realistic_v3-LHE-GEN-SIM/180403_123026/0000/",
+        "/cms/data/store/user/klo/DarkPhoton_Moriond17_GEN-SIM/v2/ZD_UpTo0j_MZD20_Eps1e-2/PUMoriond17-Realistic25ns13TeVEarly2017Collision-93X_mc2017_realistic_v3-LHE-GEN-SIM/180403_154314/0000/",
         "DarkZTo4l_v1",
         inUFTier2 = True,
         exclude = "inLHE",
