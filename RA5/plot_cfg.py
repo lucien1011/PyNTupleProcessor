eef8d723951d5276201023c0a3987205268ec707
lucien1011 <lucien1011@gmail.com>
2018-05-15 11:35:41 -0400

    added plotter and XSWeighter

--- RA5/plot_cfg.py
+++ RA5/plot_cfg.py
@@ -3,37 +3,40 @@ from Core.Sequence import Sequence
 from Core.OutputInfo import OutputInfo 
 from Core.EndSequence import EndSequence
 
-from RA5.Dataset.NanoAOD_2016MC import *
+from RA5.Dataset.NanoAOD.Run2016 import * 
 
-#from RPV.Producer.PhysObjProducer import mediumMuonProducer,looseMuonProducer,mediumElectronProducer,looseElectronProducer,jetProducer
-#from RPV.Analyzer.Plotter.Plotter import Plotter
-#from RPV.Analyzer.Plotter.PlotDefinition import allPlots
+from RA5.Weighter.XSWeighter import XSWeighter
 
-nCores = 1 
-outputDir = "/raid/raid7/lucien/SUSY/RA5/Log/Test_UF-PyNTupleRunner/2018-05-11/test1/"
+from RA5.Producer.PhysObjProducer import mediumMuonProducer,looseMuonProducer,mediumElectronProducer,looseElectronProducer,jetProducer
+
+from Plotter.Plotter import Plotter
+from Plotter.PlotEndModule import PlotEndModule
+from RA5.Config.Plotter.PlotDefinition import allPlots
+
+nCores = 4 
+outputDir = "/raid/raid7/lucien/SUSY/RA5/Log/MCDistributions/2018-05-14/AllMCSamples_v1/"
 nEvents = -1
-disableProgressBar = False
+disableProgressBar = False 
+justEndSequence = False 
+componentList = [WZTo3LNu] 
+#componentList = allMCSamples
+for dataset in componentList:
+    dataset.lumi = 35.9
 
 sequence = Sequence()
-#sequence.add(mediumMuonProducer)
-#sequence.add(looseMuonProducer)
-#sequence.add(mediumElectronProducer)
-#sequence.add(looseElectronProducer)
-#sequence.add(jetProducer)
-#sequence.add(Plotter("Plotter",allPlots))
+xsWeighter = XSWeighter("XSWeighter")
+sequence.add(xsWeighter)
+sequence.add(mediumMuonProducer)
+sequence.add(looseMuonProducer)
+sequence.add(mediumElectronProducer)
+sequence.add(looseElectronProducer)
+sequence.add(jetProducer)
+sequence.add(Plotter("Plotter",allPlots))
 
 endSequence = EndSequence()
+endModuleOutputDir = "/home/lucien/public_html/SUSY/RA5/Log/MCDistributions/2018-05-14/AllMCSamples_v1/"
+endSequence.add(PlotEndModule(endModuleOutputDir,allPlots))
 
 outputInfo = OutputInfo("OutputInfo")
 outputInfo.outputDir = outputDir
-outputInfo.TFileName = "test1.root"
-
-componentList = [
-        DYJetsToLL_M10to50,
-        #TTJetsDiLep_cmp,
-        #TTJetsSingleLepFromT_cmp,
-        #TTJetsSingleLepFromTbar_cmp,
-        #WJetsToLNu_cmp,
-        #T1tbs2000_cmp,
-        ]
-
+outputInfo.TFileName = "MCDistributions.root"
