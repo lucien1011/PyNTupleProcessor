34bfffe6ec81f85e971df8f70dec3d676fdcdaff
kun shi <shikun1@163.com>
2018-06-12 10:09:06 -0400

    Jun 12 2018

--- RPV/Producer/AnalysisProducer.py
+++ RPV/Producer/AnalysisProducer.py
@@ -1,4 +1,5 @@
 from Core.Module import Module
+import numpy
 
 class AnalysisProducer(Module):
     def analyze(self,event):
@@ -26,5 +27,44 @@ class AnalysisProducer(Module):
             event.mll = vecSum.M()
         else:
             event.mll = None
+ 
+        k1 = numpy.sqrt(numpy.square(numpy.sqrt(numpy.square(event.jets[0].pt)+numpy.square(event.jets[0].mass))+numpy.sqrt(numpy.square(event.leps[0].pt)+numpy.square(event.leps[0].mass))) - numpy.square(event.jets[0].pt-event.leps[0].pt))
+        k2 = numpy.sqrt(numpy.square(numpy.sqrt(numpy.square(event.jets[1].pt)+numpy.square(event.jets[1].mass))+numpy.sqrt(numpy.square(event.leps[1].pt)+numpy.square(event.leps[1].mass))) - numpy.square(event.jets[1].pt-event.leps[1].pt))
+        k3 = numpy.sqrt(numpy.square(numpy.sqrt(numpy.square(event.jets[0].pt)+numpy.square(event.jets[0].mass))+numpy.sqrt(numpy.square(event.leps[1].pt)+numpy.square(event.leps[1].mass))) - numpy.square(event.jets[0].pt-event.leps[1].pt))
+        k4 = numpy.sqrt(numpy.square(numpy.sqrt(numpy.square(event.jets[1].pt)+numpy.square(event.jets[1].mass))+numpy.sqrt(numpy.square(event.leps[0].pt)+numpy.square(event.leps[0].mass))) - numpy.square(event.jets[1].pt-event.leps[0].pt))
         
+        if k1 >= k2:
+           event.m0_bl[0] = k1
+           event.m1_bl[0] = k2
+        else:
+            event.m0_bl[0] = k2
+            event.m1_bl[0] = k1
+
+        if k3 >= k4:
+           event.m0_bl[1] = k3
+           event.m1_bl[1] = k4
+        else:
+            event.m0_bl[1] = k4
+            event.m1_bl[1] = k3
+
+        temp1 = (event.m0_bl[0] - event.m1_bl[0])/(event.m0_bl[0] + event.m1_bl[0])
+        temp2 = (event.m0_bl[1] - event.m1_bl[1])/(event.m0_bl[1] + event.m1_bl[1])
+
+        if temp1 < temp2:
+           event.m_asym_bl = temp1
+        else:
+            event.m_asym_bl = temp2
+
+
+
+
+
+
+
+
+
+
+
+
+
         return True
