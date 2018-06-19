a652ddca2b0db92835e5ee81649e81d6702d0d63
lucien1011 <lucien1011@gmail.com>
2018-06-19 04:55:57 -0400

    Added and updated skimmers

--- RPV/Skimmer/EventSkimmer.py
+++ RPV/Skimmer/EventSkimmer.py
@@ -1,13 +1,23 @@
 from Core.Module import Module
 
 class EventSkimmer(Module):
-    def __init__(self,name,csvCut=0.8484):
+    def __init__(self,name,csvCut=0.8484,cutflow="LooseSignal"):
         self.csvCut = csvCut
+        self.cutflow = cutflow
 
     def analyze(self,event):
-        if event.nLep40 < 2: return False
+        if self.cutflow == "LooseSignal":
+            return self.makeCommonSelection(event)
+        elif self.cutflow == "htCR":
+            return self.makeCommonSelection(event) and event.ht40 < 800.
+        else:
+            raise RuntimeError, "Cutflow "+self.cutflow+" not supported"
+
+    def makeCommonSelection(self,event):
         if event.nJet40 < 2: return False
+        if event.nLep40 < 2: return False
         if event.jets[0].btagCSVV2 < self.csvCut and event.jets[1].btagCSVV2 < self.csvCut: return False
         if event.leps[0].charge == event.leps[1].charge: return False
         if event.mll and event.mll < 300: return False
         return True
+
---
+++ RPV/Skimmer/HLTSkimmer.py
@@ -0,0 +1,8 @@
+from Core.Module import Module
+
+class HLTSkimmer(Module):
+    def __init__(self,name,cutflow="LooseSignal"):
+        self.cutflow = cutflow
+
+    def analyze(self,event):
+        return event.HLT_IsoMu22[0] or event.HLT_IsoTkMu22[0] or event.HLT_IsoMu24[0] or event.HLT_IsoTkMu24[0]
