1f440f5fb582a29db1fafd28bb5c1a75eb582e18
lucien1011 <lucien1011@gmail.com>
2018-06-24 19:09:37 -0400

    Update Heppy sample for RA5

--- DataMC/Heppy/Run2016/MC.py
+++ DataMC/Heppy/Run2016/MC.py
@@ -3,15 +3,18 @@ from Core.Utils.UFTier2Utils import *
 
 inUFTier2   = True
 treeName    = "tree"
+fdTreeName  = "sf/t"
 
 componentList = []
 sampleFileNames = listdir_uberftp(bkg_dir)
+sampleFdFilesNames = listdir_uberftp(bkg_fd_dir)
 for sampleFileName in sampleFileNames:
     if not sampleFileName.endswith(".root"): continue
     dir_path    = bkg_dir+sampleFileName
-    sampleName  = sampleFileName.replace(".root","")
+    sampleName  = sampleFileName.replace(".root","") 
+    fdFileName = "evVarFriend_"+sampleName+".root"
     cmpList = ComponentList(
-            [Component(sampleName,dir_path,treeName,inUFTier2)]
+            [Component(sampleName,dir_path,treeName,inUFTier2,fdPaths=[(bkg_fd_dir+fdFileName,fdTreeName),])]
             )
     tmpDataset = Dataset(sampleName,cmpList,)
     componentList.append(tmpDataset)
