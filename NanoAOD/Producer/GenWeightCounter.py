9d2acad3b18925a1d647b5ed1890c51f153c5d29
lucien1011 <lucien1011@gmail.com>
2018-07-04 06:36:43 -0400

    Added postfix in GenWeightCounter

--- NanoAOD/Producer/GenWeightCounter.py
+++ NanoAOD/Producer/GenWeightCounter.py
@@ -3,11 +3,15 @@ import ROOT
 from Core.Module import Module
 
 class GenWeightCounter(Module):
+    def __init__(self,name,postfix=""):
+        super(GenWeightCounter,self).__init__(name)
+        self.postfix = postfix
+
     def begin(self):
-        self.writer.book("SumWeight","TH1D","SumWeight","",1,-0.5,0.5)
-        self.writer.book("EventCount","TH1D","EventCount","",1,-0.5,0.5)
+        self.writer.book("SumWeight"+self.postfix,"TH1D","SumWeight"+self.postfix,"",1,-0.5,0.5)
+        self.writer.book("EventCount"+self.postfix,"TH1D","EventCount"+self.postfix,"",1,-0.5,0.5)
         
     def analyze(self,event):
-        self.writer.objs["SumWeight"].Fill(0.,event.genWeight[0])
-        self.writer.objs["EventCount"].Fill(0.)
+        self.writer.objs["SumWeight"+self.postfix].Fill(0.,event.genWeight[0])
+        self.writer.objs["EventCount"+self.postfix].Fill(0.)
         return True
