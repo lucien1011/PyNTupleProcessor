30678765972a9cfe6d68eb0205f651e730613dd1
lucien1011 <lucien1011@gmail.com>
2018-07-04 19:39:36 -0400

    Added Run2016 MC

---
+++ DarkZ/Dataset/Run2016/MC.py
@@ -0,0 +1,21 @@
+from Core.ComponentList import *
+from Core.Dataset import Dataset
+
+bkgTreeDir      = "/cms/data/store/user/t2/users/dsperka/Run2/HZZ4l/SubmitArea_13TeV/rootfiles_MC80X_4lskim_M17_Feb21/"
+inUFTier2       = True
+
+# ____________________________________________________________________________________________________________________________________________ ||
+# ggH
+ggH_cmpList = ComponentList(
+        [ 
+            Component("ggH",bkgTreeDir+"GluGluHToZZTo4L_M125_13TeV_powheg2_JHUgenV6_pythia8_RunIISummer16MiniAODv2.root","Ana/passedEvents",inUFTier2=inUFTier2),
+        ]
+        )
+
+ggH = Dataset(
+        "ggH",
+        ggH_cmpList,
+        isMC                = True,
+        xs                  = 0.01218,
+        )
+ggH.setSumWeight(bkgTreeDir+"GluGluHToZZTo4L_M125_13TeV_powheg2_JHUgenV6_pythia8_RunIISummer16MiniAODv2.root","Ana/sumWeights",inUFTier2)
