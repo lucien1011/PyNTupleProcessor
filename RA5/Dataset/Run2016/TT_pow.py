b7f74ec6520c670640aa182dd04d6699bf200204
kun shi <shikun1@163.com>
2018-07-19 18:06:11 -0400

    19Jul2018

--- RA5/Dataset/Run2016/TT_pow.py
+++ RA5/Dataset/Run2016/TT_pow.py
@@ -4,13 +4,17 @@ from Core.Utils.MakeComponent import makeComponents
 from DataMC.Heppy.Run2016.HaddMC import TT_pow
 
 sampleName   = "TT_pow"
-#fileName     = "treeProducerSusyRA5.root"
-#TreeDir      = "/cms/data/store/user/t2/users/klo/HeppyTree/heppy_80X_RA5_Legacy/July18_v1_LeptonJetRecleaner_TT_pow/"
+fileName     = "treeProducerSusyRA5.root"
+TreeDir      = "/cms/data/store/user/t2/users/klo/HeppyTree/heppy_80X_RA5_Legacy/July18_v1_LeptonJetRecleaner_TT_pow/"
 #sumweight_path  = "/cms/data/store/user/t2/users/klo/HeppyTree/heppy_80X_RA5_Legacy/July18_v1/"
-#inUFTier2    = True
+inUFTier2    = True
 #filePath = os.path.join(sumweight_path,sampleName,fileName)
 
 #cmp = makeComponents(sampleName, TreeDir, "Events", inUFTier2)
+
+cmpList = ComponentList(
+                       [ Component("TT_pow",TreeDir + sampleName + "/"+"TT_pow_%s_SkimTree.root"%i,"tree",inUFTier2) ]
+        )
 
 TT_pow.componentList = cmpList
 
