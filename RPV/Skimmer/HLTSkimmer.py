f0b19b2e6035576cc041953971a622aa10e7aa62
kun shi <shikun1@163.com>
2018-07-20 09:47:53 -0400

    20Jul2018

--- RPV/Skimmer/HLTSkimmer.py
+++ RPV/Skimmer/HLTSkimmer.py
@@ -6,4 +6,8 @@ class HLTSkimmer(Module):
         self.cutflow = cutflow
 
     def analyze(self,event):
-        return event.HLT_IsoMu22[0] or event.HLT_IsoTkMu22[0] or event.HLT_IsoMu24[0] or event.HLT_IsoTkMu24[0]
+        if(event == "%s" + "SingleMuon" +"%s"):
+          return event.HLT_IsoMu22[0] or event.HLT_IsoTkMu22[0] or event.HLT_IsoMu24[0] or event.HLT_IsoTkMu24[0]
+        if(event == "%s" + "SingleElectron" +"%s"):
+          return event.HLT_Ele27_eta2p1_WPTight_Gsf[0]
+
