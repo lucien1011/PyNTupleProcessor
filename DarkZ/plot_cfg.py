a94c1677a6fb5ebe638bc8748c5a0ed781377bad
lucien1011 <lucien1011@gmail.com>
2018-07-26 09:45:45 -0400

    Change naming

--- DarkZ/plot_cfg.py
+++ DarkZ/plot_cfg.py
@@ -21,39 +21,39 @@ from Plotter.Plot import Plot
 #out_path = "MCDistributions/MC_BaselineSelection_v1/2018-07-09/"
 out_path = "MCDistributions/MC_HiggsTo4LSelection_v1/2018-07-26/"
 
-muon_plots = [
-        Plot("Muon1_Pt", ["TH1D","Muon1_pt","",20,0.,200.], LambdaFunc('x: x.Z1.lep1.vec.Pt()'),),
-        Plot("Muon2_Pt", ["TH1D","Muon2_pt","",20,0.,100.], LambdaFunc('x: x.Z1.lep2.vec.Pt()'),),
-        Plot("Muon3_Pt", ["TH1D","Muon3_pt","",20,0.,100.], LambdaFunc('x: x.Z2.lep1.vec.Pt()'),),
-        Plot("Muon4_Pt", ["TH1D","Muon4_pt","",20,0.,50.],  LambdaFunc('x: x.Z2.lep2.vec.Pt()'),),
+lepton_plots = [
+        Plot("Lepton1_Pt", ["TH1D","Lepton1_pt","",20,0.,200.], LambdaFunc('x: x.Z1.lep1.vec.Pt()'),),
+        Plot("Lepton2_Pt", ["TH1D","Lepton2_pt","",20,0.,100.], LambdaFunc('x: x.Z1.lep2.vec.Pt()'),),
+        Plot("Lepton3_Pt", ["TH1D","Lepton3_pt","",20,0.,100.], LambdaFunc('x: x.Z2.lep1.vec.Pt()'),),
+        Plot("Lepton4_Pt", ["TH1D","Lepton4_pt","",20,0.,50.],  LambdaFunc('x: x.Z2.lep2.vec.Pt()'),),
         
-        Plot("Muon1_Eta", ["TH1D","Muon1_eta","",20,-3.,3.], LambdaFunc('x: x.Z1.lep1.vec.Eta()'),),
-        Plot("Muon2_Eta", ["TH1D","Muon2_eta","",20,-3.,3.], LambdaFunc('x: x.Z1.lep2.vec.Eta()'),),
-        Plot("Muon3_Eta", ["TH1D","Muon3_eta","",20,-3.,3.], LambdaFunc('x: x.Z2.lep1.vec.Eta()'),),
-        Plot("Muon4_Eta", ["TH1D","Muon4_eta","",20,-3.,3.], LambdaFunc('x: x.Z2.lep2.vec.Eta()'),),
+        Plot("Lepton1_Eta", ["TH1D","Lepton1_eta","",20,-3.,3.], LambdaFunc('x: x.Z1.lep1.vec.Eta()'),),
+        Plot("Lepton2_Eta", ["TH1D","Lepton2_eta","",20,-3.,3.], LambdaFunc('x: x.Z1.lep2.vec.Eta()'),),
+        Plot("Lepton3_Eta", ["TH1D","Lepton3_eta","",20,-3.,3.], LambdaFunc('x: x.Z2.lep1.vec.Eta()'),),
+        Plot("Lepton4_Eta", ["TH1D","Lepton4_eta","",20,-3.,3.], LambdaFunc('x: x.Z2.lep2.vec.Eta()'),),
         
-        Plot("Muon1_Phi", ["TH1D","Muon1_phi","",20,-5.,5.], LambdaFunc('x: x.Z1.lep1.vec.Phi()'),),
-        Plot("Muon2_Phi", ["TH1D","Muon2_phi","",20,-5.,5.], LambdaFunc('x: x.Z1.lep2.vec.Phi()'),),
-        Plot("Muon3_Phi", ["TH1D","Muon3_phi","",20,-5.,5.], LambdaFunc('x: x.Z2.lep1.vec.Phi()'),),
-        Plot("Muon4_Phi", ["TH1D","Muon4_phi","",20,-5.,5.], LambdaFunc('x: x.Z2.lep2.vec.Phi()'),),
+        Plot("Lepton1_Phi", ["TH1D","Lepton1_phi","",20,-5.,5.], LambdaFunc('x: x.Z1.lep1.vec.Phi()'),),
+        Plot("Lepton2_Phi", ["TH1D","Lepton2_phi","",20,-5.,5.], LambdaFunc('x: x.Z1.lep2.vec.Phi()'),),
+        Plot("Lepton3_Phi", ["TH1D","Lepton3_phi","",20,-5.,5.], LambdaFunc('x: x.Z2.lep1.vec.Phi()'),),
+        Plot("Lepton4_Phi", ["TH1D","Lepton4_phi","",20,-5.,5.], LambdaFunc('x: x.Z2.lep2.vec.Phi()'),),
             
         ]
 
 general_plots = [
-        Plot("Z1_mass",     ["TH1D","Z1_mass","",30,0.,150.],   LambdaFunc('x: x.Z1.vec.M()'),       ),
-        Plot("Z2_mass",     ["TH1D","Z2_mass","",30,0.,150.],   LambdaFunc('x: x.Z2.vec.M()'),       ),
-        Plot("h4L_mass",    ["TH1D","4L_mass","",25,100.,150.], LambdaFunc('x: x.hmass'),           ),
-        Plot("Z1mass_vs_Z2mass",["TH2D","Z1mass_vs_Z2mass","",30,0.,150.,30,0.,150.], LambdaFunc('x: [x.Z1.vec.M(),x.Z2.vec.M(),]'),dim=2),
-        #Plot("h4L_Pt",      ["TH1D","4L_Pt","",40,0.,200.],     LambdaFunc('x: x.pT4l[0]'),         ),
-        #Plot("met",         ["TH1D","met","",40,0.,200.],       LambdaFunc('x: x.met[0]'),          ),
-        #Plot("nVtx",        ["TH1D","nVtx","",30,0.0,60.0],      LambdaFunc('x: x.nVtx[0]')),
+        Plot("Z1_mass",         ["TH1D","Z1_mass","",110,40.,110.],   LambdaFunc('x: x.Z1.vec.M()'),       ),
+        Plot("Z2_mass",         ["TH1D","Z2_mass","",100,0.,65.],   LambdaFunc('x: x.Z2.vec.M()'),       ),
+        Plot("h4L_mass",        ["TH1D","4L_mass","",25,110.,140.], LambdaFunc('x: x.hmass'),           ),
+        Plot("h4L_Pt",          ["TH1D","4L_Pt","",40,0.,200.],     LambdaFunc('x: x.pT4l[0]'),         ),
+        Plot("met",             ["TH1D","met","",40,0.,200.],       LambdaFunc('x: x.met[0]'),          ),
+        Plot("nVtx",            ["TH1D","nVtx","",30,0.0,60.0],      LambdaFunc('x: x.nVtx[0]')),        
+        Plot("Z1mass_vs_Z2mass",["TH2D","Z1mass_vs_Z2mass","",110,40.,110.,100,0.,65.], LambdaFunc('x: [x.Z1.vec.M(),x.Z2.vec.M(),]'),dim=2),
         ]
 
 jet_plots = [
         #Plot("nJet",    ["TH1D","nJet","",5,-0.5,4.5],      LambdaFunc('x: x.njets_pt30_eta2p5[0]'),     ),
         ]
 
-plots = muon_plots + general_plots + jet_plots
+plots = lepton_plots + general_plots + jet_plots
 for plot in plots:
     plot.plotSetting.divideByBinWidth = True
 
@@ -62,7 +62,7 @@ outputDir               = "/raid/raid7/lucien/Higgs/DarkZ/"+out_path
 nEvents                 = -1
 disableProgressBar      = False
 componentList           = bkgSamples + sigSamples
-justEndSequence         = True
+justEndSequence         = False
 
 for dataset in componentList:
     if dataset.isMC:
@@ -74,7 +74,7 @@ sequence                = Sequence()
 xsWeighter              = XSWeighter("XSWeighter")
 recoSkimmer             = RecoSkimmer("RecoSkimmer")
 #recoSkimmer.Z1MassRange = [80.,100.]
-#recoSkimmer.Z2MassRange = [15.,115.]
+recoSkimmer.Z2MassRange = [4.,120.]
 #recoSkimmer.m4lCut      = [115.,130.]
 plotter                 = Plotter("Plotter",plots)
 
