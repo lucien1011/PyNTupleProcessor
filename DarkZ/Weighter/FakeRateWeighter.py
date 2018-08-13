039aea47519dc25c7f8c2d365b35c2c630ceae16
lucien1011 <lucien1011@gmail.com>
2018-08-12 20:44:52 -0400

    Change FakeRateWeighter

--- DarkZ/Weighter/FakeRateWeighter.py
+++ DarkZ/Weighter/FakeRateWeighter.py
@@ -1,18 +1,16 @@
 from Core.Module import Module
 import ROOT
 
-el_histName_br = "h1D_FRel_EB"
-el_histName_ec = "h1D_FRel_EE"
-mu_histName_br = "h1D_FRmu_EB"
-mu_histName_ec = "h1D_FRmu_EE"
-
 class FakeRateWeighter(Module):
     def analyze(self,event):
         if "ZPlusX" in self.dataset.name:
-            if event.nZXCRFailedLeptons[0] == 1:
-                event.weight *= -1*event.FRWeight[0]
-            elif event.nZXCRFailedLeptons[0] == 2:
+            #if event.nZXCRFailedLeptons[0] == 1:
+            if event.FRWeight == -1.: continue
+            if event.nFailedLeptonsZ2[0] == 1:
                 event.weight *= event.FRWeight[0]
-            #else:
-            #    return False
-        return True 
+            #elif event.nZXCRFailedLeptons[0] == 2:
+            elif event.nFailedLeptonsZ2[0] == 2:
+                event.weight *= -1*event.FRWeight[0]
+            else:
+                return False
+        return True
