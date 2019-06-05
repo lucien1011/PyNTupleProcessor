1400e76896670f5b30bd07f311ea2bbdf94502e8
lucien1011 <lucien1011@gmail.com>
2019-06-05 08:37:26 -0400

    Added more samples

--- Zprime/Dataset/Run2017/SkimTree_Zprime_m4l70.py
+++ Zprime/Dataset/Run2017/SkimTree_Zprime_m4l70.py
@@ -3,7 +3,7 @@ from Core.Dataset import Dataset
 from Utils.System import system
 from Utils.SumWeight import handleSumWeight
 
-bkgSkimTreeDir      = system.getStoragePath()+"/lucien/Higgs/Zprime-NTuple/20190510/SkimTree_Zprime_Run2017Data_m4l70/"
+bkgSkimTreeDir      = system.getStoragePath()+"/lucien/Higgs/Zprime-NTuple/20190605/SkimTree_Zprime_Run2017Data_m4l70/"
 bkgSkimTreeDir2     = bkgSkimTreeDir
 bkgTreeDir          = "/cms/data/store/user/muahmad/rootfiles_2017/"
 dataTreeDir         = bkgSkimTreeDir
