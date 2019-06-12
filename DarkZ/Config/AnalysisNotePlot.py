93e22740e8f20ee2f9c86b8551c332118e720883
Lucien <lucien@luciens-macbook-pro.home>
2019-06-12 14:53:01 +0200

    Preapproval plots

--- DarkZ/Config/AnalysisNotePlot.py
+++ DarkZ/Config/AnalysisNotePlot.py
@@ -4,14 +4,14 @@ import array
 
 # ________________________________________________________________________ ||
 mZ1PlotRange        = [60-1,array.array('d',[40.*1.02**i for i in range(60)]),]
-#mZ2PlotRange        = [180-1,array.array('d',[4.*1.02**i for i in range(180)]),]
-#mZ2LowM4lPlotRange  = [140-1,array.array('d',[4.*1.02**i for i in range(140)]),]
-#mZ2MidM4lPlotRange  = [140-1,array.array('d',[4.*1.02**i for i in range(140)]),]
-#mZ2HighM4lPlotRange = [152-1,array.array('d',[4.*1.02**i for i in range(152)]),]
-mZ2PlotRange        = [38,4.,80.,]
-mZ2LowM4lPlotRange  = [28,4.,60.,]
-mZ2MidM4lPlotRange  = [28,4.,60.,]
-mZ2HighM4lPlotRange = [38,4.,80.,]
+mZ2PlotRange        = [155-1,array.array('d',[4.*1.02**i for i in range(155)]),]
+mZ2LowM4lPlotRange  = [140-1,array.array('d',[4.*1.02**i for i in range(140)]),]
+mZ2MidM4lPlotRange  = [140-1,array.array('d',[4.*1.02**i for i in range(140)]),]
+mZ2HighM4lPlotRange = [155-1,array.array('d',[4.*1.02**i for i in range(155)]),]
+#mZ2PlotRange        = [38,4.,80.,]
+#mZ2LowM4lPlotRange  = [28,4.,60.,]
+#mZ2MidM4lPlotRange  = [28,4.,60.,]
+#mZ2HighM4lPlotRange = [38,4.,80.,]
 h4lPlotRange        = [47-1,array.array('d',[80.*1.02**i for i in range(47)]),]
 
 # ________________________________________________________________________ ||
--- DarkZ/plot_DarkPhoton_SR_RunII_cfg.py
+++ DarkZ/plot_DarkPhoton_SR_RunII_cfg.py
@@ -15,18 +15,31 @@ from Plotter.PlotEndModule import PlotEndModule
 
 from DarkZ.Config.MergeSampleDict_RunII import mergeSampleDict
 
-import os
+import os,ROOT
 
 User                    = os.environ['USER']
-out_path                = "DarkPhotonSR/DataMCDistributions/2019-05-24_RunII/"
+#out_path                = "DarkPhotonSR/DataMCDistributions/2019-05-24_RunII/"
+out_path                = "DarkPhotonSR/DataMCDistributions/2019-06-12_RunII/"
 nCores                  = 3
 outputDir               = system.getStoragePath()+"/"+User+"/Higgs/DarkZ/"+out_path
 nEvents                 = -1
 disableProgressBar      = False
-componentList           = bkgSamples
-justEndSequence         = False
+#componentList           = bkgSamples + sigSamples + ppZZdSamples
+componentList           = bkgSamples + [sample2016.HZZd_M15,sample2016.HZZd_M30] + [sample_ppZZd.ppZZd4l_M15,sample_ppZZd.ppZZd4l_M30]
+justEndSequence         = True
 
 plots = general_4e_plots + general_2mu2e_plots + general_4mu_plots + general_2e2mu_plots
+
+for sig in sigSamples:
+    for p in plots:
+        p.plotSetting.line_style_dict[sig.name] = 10
+        p.plotSetting.line_width_dict[sig.name] = 4
+        p.plotSetting.line_color_dict[sig.name] = ROOT.kRed
+for sig in ppZZdSamples:
+    for p in plots:
+        p.plotSetting.line_style_dict[sig.name] = 9
+        p.plotSetting.line_width_dict[sig.name] = 4
+        p.plotSetting.line_color_dict[sig.name] = ROOT.kOrange
 
 for dataset in componentList:
     for component in dataset.componentList:
