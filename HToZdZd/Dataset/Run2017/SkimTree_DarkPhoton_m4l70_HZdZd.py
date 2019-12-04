29f1e6f6d884b60eed9ea9689d1cf72fd0158f32
lucien1011 <lucien1011@gmail.com>
2019-12-04 05:44:55 -0500

    All mass points in HZdZd

--- HToZdZd/Dataset/Run2017/SkimTree_DarkPhoton_m4l70_HZdZd.py
+++ HToZdZd/Dataset/Run2017/SkimTree_DarkPhoton_m4l70_HZdZd.py
@@ -17,7 +17,6 @@ fileNameTemplate        = "HToZdZdTo4L_M125_MZd%s_eps2e-2_kap1e-4_TuneCP5_13TeV_
 datasetName             = "HToZdZd_M%s"
 skimTreePath            = "passedEvents"
 mass_points             = [4,5,6,7,8,9,10,15,20,25,30,35,40,45,50,55,60,]
-#mass_points             = [4,5,6,7,8,9,10,15,20,25,30,35,40,45,55,]
 
 # ____________________________________________________________________________________________________________________________________________ ||
 sigSampleDict = {}
