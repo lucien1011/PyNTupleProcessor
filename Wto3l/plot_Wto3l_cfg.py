d64e4578800c9d43acbdef9b5402f9c896eec7eb
lucien1011 <lucien1011@gmail.com>
2020-09-01 11:29:07 -0400

    Comment out old lines

--- Wto3l/plot_Wto3l_cfg.py
+++ Wto3l/plot_Wto3l_cfg.py
@@ -20,29 +20,29 @@ sel_3mu = "x.lep_id.size() >= 3 and x.lep_pt.size() >= 3"
 # __________________________________________________________________________________ ||
 
 general_plots.extend([
-    Plot("Lep1_pt",          ["TH1D","Lep1_pt","",40,0.,200.],           LambdaFunc('x:x.Lep1.Pt()'),	selFunc=LambdaFunc('x:x.Lep1.Pt() > 0')        ),
-    Plot("Lep2_pt",          ["TH1D","Lep2_pt","",40,0.,200.],           LambdaFunc('x:x.Lep2.Pt()'),	selFunc=LambdaFunc('x:x.Lep2.Pt() > 0')        ),
-    Plot("Lep3_pt",          ["TH1D","Lep3_pt","",200,0.,200.],          LambdaFunc('x:x.Lep3.Pt()'),	selFunc=LambdaFunc('x:x.Lep3.Pt > 0')        ),
+    #Plot("Lep1_pt",          ["TH1D","Lep1_pt","",40,0.,200.],           LambdaFunc('x:x.Lep1.Pt()'),	selFunc=LambdaFunc('x:x.Lep1.Pt() > 0')        ),
+    #Plot("Lep2_pt",          ["TH1D","Lep2_pt","",40,0.,200.],           LambdaFunc('x:x.Lep2.Pt()'),	selFunc=LambdaFunc('x:x.Lep2.Pt() > 0')        ),
+    #Plot("Lep3_pt",          ["TH1D","Lep3_pt","",200,0.,200.],          LambdaFunc('x:x.Lep3.Pt()'),	selFunc=LambdaFunc('x:x.Lep3.Pt > 0')        ),
 
-    Plot("Lep1_eta",         ["TH1D","Lep1_eta","",60,-3.,3.],           LambdaFunc('x:x.Lep1.Eta()'),  selFunc=LambdaFunc('x:x.Lep1.Eta()')        ),
-    Plot("Lep2_eta",         ["TH1D","Lep2_eta","",60,-3.,3.],           LambdaFunc('x:x.Lep2.Eta()'),  selFunc=LambdaFunc('x:x.Lep2.Eta()')        ),
-    Plot("Lep3_eta",         ["TH1D","Lep3_eta","",60,-3.,3.],           LambdaFunc('x:x.Lep3.Eta()'),	selFunc=LambdaFunc('x:x.Lep3.Eta()')        ),
+    #Plot("Lep1_eta",         ["TH1D","Lep1_eta","",60,-3.,3.],           LambdaFunc('x:x.Lep1.Eta()'),  selFunc=LambdaFunc('x:x.Lep1.Eta()')        ),
+    #Plot("Lep2_eta",         ["TH1D","Lep2_eta","",60,-3.,3.],           LambdaFunc('x:x.Lep2.Eta()'),  selFunc=LambdaFunc('x:x.Lep2.Eta()')        ),
+    #Plot("Lep3_eta",         ["TH1D","Lep3_eta","",60,-3.,3.],           LambdaFunc('x:x.Lep3.Eta()'),	selFunc=LambdaFunc('x:x.Lep3.Eta()')        ),
 
-    Plot("Lep1_phi",         ["TH1D","Lep1_phi","",40,-4.,4.],           LambdaFunc('x:x.Lep1.Phi()'),  selFunc=LambdaFunc('x:x.Lep1.Phi()')        ),
-    Plot("Lep2_phi",         ["TH1D","Lep2_phi","",40,-4.,4.],           LambdaFunc('x:x.Lep2.Phi()'),  selFunc=LambdaFunc('x:x.Lep2.Phi()')        ),
-    Plot("Lep3_phi",         ["TH1D","Lep3_phi","",40,-4.,4.],           LambdaFunc('x:x.Lep3.Phi()'),  selFunc=LambdaFunc('x:x.Lep3.Phi()')        ),
+    #Plot("Lep1_phi",         ["TH1D","Lep1_phi","",40,-4.,4.],           LambdaFunc('x:x.Lep1.Phi()'),  selFunc=LambdaFunc('x:x.Lep1.Phi()')        ),
+    #Plot("Lep2_phi",         ["TH1D","Lep2_phi","",40,-4.,4.],           LambdaFunc('x:x.Lep2.Phi()'),  selFunc=LambdaFunc('x:x.Lep2.Phi()')        ),
+    #Plot("Lep3_phi",         ["TH1D","Lep3_phi","",40,-4.,4.],           LambdaFunc('x:x.Lep3.Phi()'),  selFunc=LambdaFunc('x:x.Lep3.Phi()')        ),
 
-    Plot("3lep_pt",          ["TH1D","3lep_pt","",40,0.,200.],           LambdaFunc('x:x.pT3l[0]'),	selFunc=LambdaFunc('x:x.pT3l[0] > 0')        ),
-    Plot("Lep1+Lep2_pt",     ["TH1D","Lep1+Lep2_pt","",40,0.,200.],      LambdaFunc('x:x.twolpt'),	selFunc=LambdaFunc('x:x.twolpt > 0')      ),
-    Plot("Mass1",            ["TH1D","Mass1","",80,0.,200.],             LambdaFunc('x:x.mass1'),       selFunc=LambdaFunc('x:x.mass1 > 0')        ),
-    Plot("Mass2",            ["TH1D","Mass2","",80,0.,200.],             LambdaFunc('x:x.mass2'),       selFunc=LambdaFunc('x:x.mass2 > 0')        ),
-    Plot("Transverse_Mass",  ["TH1D","Transverse_Mass","",100,0.,500.],            LambdaFunc('x:x.mt'),                selFunc=LambdaFunc('x:x.mt')        ),
-    Plot("Mass1vsMass2",     ["TH2D","Mass1_vs_Mass2","",80,0.,200.,80,0.,200.],   LambdaFunc('x:[x.mass1,x.mass2]'),	selFunc=LambdaFunc('x:[x.mass1 > 0,x.mass2 > 0]'),   dim = 2 ),
+    #Plot("3lep_pt",          ["TH1D","3lep_pt","",40,0.,200.],           LambdaFunc('x:x.pT3l[0]'),	selFunc=LambdaFunc('x:x.pT3l[0] > 0')        ),
+    #Plot("Lep1+Lep2_pt",     ["TH1D","Lep1+Lep2_pt","",40,0.,200.],      LambdaFunc('x:x.twolpt'),	selFunc=LambdaFunc('x:x.twolpt > 0')      ),
+    #Plot("Mass1",            ["TH1D","Mass1","",80,0.,200.],             LambdaFunc('x:x.mass1'),       selFunc=LambdaFunc('x:x.mass1 > 0')        ),
+    #Plot("Mass2",            ["TH1D","Mass2","",80,0.,200.],             LambdaFunc('x:x.mass2'),       selFunc=LambdaFunc('x:x.mass2 > 0')        ),
+    #Plot("Transverse_Mass",  ["TH1D","Transverse_Mass","",100,0.,500.],            LambdaFunc('x:x.mt'),                selFunc=LambdaFunc('x:x.mt')        ),
+    #Plot("Mass1vsMass2",     ["TH2D","Mass1_vs_Mass2","",80,0.,200.,80,0.,200.],   LambdaFunc('x:[x.mass1,x.mass2]'),	selFunc=LambdaFunc('x:[x.mass1 > 0,x.mass2 > 0]'),   dim = 2 ),
                   
     Plot("met",              ["TH1D","met","",50,0.,250.],               LambdaFunc('x: x.met[0]'),   	selFunc=LambdaFunc('x:x.met[0] > 0 and '+sel_3mu),     ),
     Plot("met_phi",          ["TH1D","met_phi","",40,-4.,4.],            LambdaFunc('x: x.met_phi[0]'), selFunc=LambdaFunc('x:x.met_phi[0]'),     ),
 
-    Plot("IsoL3",            ["TH1D","IsoL3","",20,0.,1.],               LambdaFunc('x: x.IsoL3[0]'),   selFunc=LambdaFunc('x:x.IsoL3[0] > 0'),     ), 
+    #Plot("IsoL3",            ["TH1D","IsoL3","",20,0.,1.],               LambdaFunc('x: x.IsoL3[0]'),   selFunc=LambdaFunc('x:x.IsoL3[0] > 0'),     ), 
         ])
 
 plots =  general_plots
