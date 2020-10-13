cb2309b2e04853ba6cc3a99568685c98d73c6787
Nik Menendez <nikmenendez@ufl.edu>
2020-10-13 10:38:54 -0400

    Added weighter to sequence

--- Wto3l/Sequence/RecoSequence.py
+++ Wto3l/Sequence/RecoSequence.py
@@ -1,12 +1,18 @@
 from Core.Sequence import Sequence
 
+from DarkZ.Weighter.DataMCWeighter import DataMCWeighter
 from NanoAOD.Weighter.XSWeighter import XSWeighter # Stealing module from NanoAOD framework
 from Wto3l.Skimmer.ZSelector import ZSelector
+from Wto3l.Weighter.FakerateWeighter import FakerateWeighter
 
 xsWeighter              = XSWeighter("XSWeighter")
+dataMCWeighter          = DataMCWeighter("DataMCWeighter")
 zSelector            	= ZSelector("ZSelector")
+fakerateWeighter        = FakerateWeighter("FakerateWeighter")
 
 Wto3l_sequence = Sequence()
-Wto3l_sequence.add(zSelector)
 Wto3l_sequence.add(xsWeighter)
+Wto3l_sequence.add(dataMCWeighter)
+Wto3l_sequence.add(zSelector)
+#Wto3l_sequence.add(fakerateWeighter)
 
